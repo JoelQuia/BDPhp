5b04839523a62f8c09563b987bc691cc4c599005
JoelQuia <oliver.joel2020@gmail.com>
2024-09-27 23:31:48 -0300

    Delete PhpBanco directory

--- PhpBanco/src/models/UsuarioSistema.php
+++
@@ -1,7 +0,0 @@
-<?php
-    namespace App\models;
-
-    class UsuarioSistema{
-        public static $nome ;
-        public static $variavelestatica = "valordavar_estatica";
-    }
--- PhpBanco/src/util/globalvars.php
+++
@@ -1,5 +0,0 @@
-<?php
-    namespace App\util;
-    class globalvars{
-        public static  $nomedoApp = "alguma informação foi desconsiderada";
-    }
